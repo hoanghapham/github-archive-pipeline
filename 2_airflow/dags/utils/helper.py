fdae2e941d35688c3ddfac2188ce8ed930c4443a
hoanghapham <hoangha.20021992@gmail.com>
2022-04-02 14:55:41 +0700

    add functions to process data, and ability to parallelize functions

--- 2_airflow/dags/utils/helper.py
+++ 2_airflow/dags/utils/helper.py
@@ -1,15 +1,14 @@
 from google.cloud import storage
 from google.cloud import bigquery
-from google.oauth2 import service_account
 from google.cloud.exceptions import NotFound
 import os 
 import json
 import requests
 import gzip
-from multiprocessing import Pool
-from custom_logger import init_logger
+from billiard import Pool, Process
+from . import custom_logger
 
-logger = init_logger(__name__)
+logger = custom_logger.init_logger(__name__)
 
 def upload_to_gcs(bucket, object_name, local_file):
     """
@@ -29,6 +28,7 @@ def upload_to_gcs(bucket, object_name, local_file):
     bucket = client.bucket(bucket)
 
     blob = bucket.blob(object_name)
+    logger.info(f"Uploading {local_file} to {bucket}")
     blob.upload_from_filename(local_file)
 
 def gcs_to_bigquery_table(bucket, file_path, schema_path, destination_table_id, partition_field):
@@ -83,11 +83,11 @@ def gcs_to_bigquery_table(bucket, file_path, schema_path, destination_table_id, 
         raise Exception(f"File not found: {uri}")
         
 
-def download_files(execution_date, hour, download_dir):
+def download_file(execution_date, hour, download_dir):
     logger.info(f"Downloading data of date {execution_date} - hour {hour}")
     url = f"https://data.gharchive.org/{execution_date}-{hour}.json.gz"
     req = requests.get(url)
-    output_dir_path = f"{download_dir}/{execution_date}"
+    output_dir_path = os.path.join(download_dir, execution_date)
     os.makedirs(output_dir_path, exist_ok=True)
 
     byte_content = gzip.decompress(req.content)
@@ -96,30 +96,69 @@ def download_files(execution_date, hour, download_dir):
         f.write(byte_content)
     
 
-def filter_events(in_file_path, out_dir_path, out_suffix):
-    logger.info("Filtering events")
-    create_events = []
-    push_events = []
+def filter_events(
+        in_file_path, 
+        out_folder_path, 
+        suffix,
+        included_events = ['CreateEvent', 'PushEvent'],
+        output = ['json', 'gz', 'return'],
+    ):
+    logger.info(f"Processing file {in_file_path}")
+    events_store = {k: [] for k in included_events}
 
     with open(in_file_path, 'r') as f:
         while True:
             line = f.readline()
             if line:
                 json_line = json.loads(line)
-                if json_line['type'] == 'CreateEvent':
-                    create_events.append(json_line)
-                elif json_line['type'] == 'PushEvent':
-                    push_events.append(json_line)
+                if json_line['type'] in included_events:
+                    events_store[json_line['type']].append(json_line)
                 else:
                     next
             else:
                 break
-    with open(f"{out_dir_path}/create_events_{out_suffix}.json", 'a') as f:
-        json.dump(create_events, f)
-    with open(f"{out_dir_path}/push_events_{out_suffix}.json", 'a') as f:
-        json.dump(push_events, f)
+    if output == 'return':
+        return events_store
+    elif output == 'gz': 
+        for key, value in events_store.items():
+            with gzip.open(
+                filename=os.path.join(out_folder_path, f"{key}_{suffix}.json.gz"), 
+                mode='ab', 
+                compresslevel=5
+            ) as f:
+                f.writelines(bytes(json.dumps(i) + "\n", encoding="utf-8") for i in value)
+    elif output == 'json':
+        for key, value in events_store.items():
+            with open(os.path.join(out_folder_path, f"{key}_{suffix}.json"), 'a') as f:
+                for i in value:
+                    json.dump(i, f)
+                    f.write("\n")
+    else:
+        raise ValueError(f"Output not supported: {output}")
+    
 
 def parallel_process(func, args_list, process_num=4):
     logger.info(f"Init {process_num} parallel processes")
     with Pool(process_num) as p:
         p.starmap(func, args_list)
+
+
+def parallel_process_manual(func, args_list, process_num=4):
+    pool = {}
+    for i in range(len(args_list) // process_num):
+        logger.info(f"Processing batch {i}")
+        for pid, args in enumerate(args_list[i*process_num:min((i+1)*process_num,len(args_list))]):
+            p = Process(target=func, args=args, daemon=False)
+            pool[pid] = p
+        
+        for pid in pool.keys():
+            pool[pid].start()
+        
+        for pid in pool.keys():
+            pool[pid].join()
+            pool[pid].close()
+
+def sequential_process(func, args_list):
+    for args in args_list:
+        func(*args)
+
